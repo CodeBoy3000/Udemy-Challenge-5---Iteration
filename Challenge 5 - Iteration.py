5699cf753193107b33d13a570d44d24e577c1acb
“Codeboy3000” <iantfleming1992@gmail.com>
2019-09-03 15:46:08 +0100

    Python code

---
+++ Challenge 5 - Iteration.py
@@ -0,0 +1,32 @@
+list1 = ["cat", "dog", "bear"]
+
+iterator = iter(list1)
+
+for i in range(0, len(list1)):
+    next_item = next(iterator)
+    print(next_item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
